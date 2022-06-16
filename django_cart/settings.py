6444cadbba8197c9200635d005ceeb361d0c7056
ebikdeli <ebikdeli@gmail.com>
2022-06-16 22:21:46 +0430

    MEDIA_ROOT bug fixed

--- django_cart/settings.py
+++ django_cart/settings.py
@@ -98,6 +98,6 @@ STATICFILES_DIRS = [BASE_DIR,
 
 MEDIA_URL = 'media/'
 
-MEIDA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
